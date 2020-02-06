b8e8b797c743438885eaeb96d7f9937302bcff98
Mr.YanG <yangyonghai9125@hotmail.com>
2020-02-06 14:52:52 +0800

    feat: import webpack

--- src/search/index.js
+++ src/search/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Search extends React.Component {
-
-    render() {
-        return (
-            <div className="search-text" >
-                        123
-            </div>
-                )
-    }
+  render() {
+    return (
+      <div className="search-text" >
+        123
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-    <Search />,
-    document.getElementById('root')
+  <Search />,
+  document.getElementById('root')
 )
